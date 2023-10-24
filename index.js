2e4fa5a4c9c16bb7e9dabafa0fb32d9b2d81de0e
rpm3 <mancuso.b3@gmail.com>
2023-10-24 08:53:46 -0500

    Added question array to inquirer and initialize function

--- index.js
+++ index.js
@@ -1,2 +1,52 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+// Question array
+const questions = [
+    {
+        type: 'input',
+        message: 'Enter up to three characters for your logo: ',
+        name: 'text',
+    },
+    {
+        type: 'input',
+        message: 'Enter a text color keyword or hexadecimal (ie blue or 0000FF):',
+        name: 'text-color',
+    },
+    {
+        type: 'list',
+        message: 'Please choose a background shape:',
+        name: 'shape',
+        choices: ['Circle', 'Square', 'Triangle'],
+    },
+    {
+        type: 'input',
+        message: 'Please enter a shape color keyword or hexadecimal: ',
+        name: 'shape-color',
+    },
+];
+
+// // Function to import generateMarkdown and run answers through it
+// function writeToFile(fileName, answers) {
+//     // console.log('writetofile', answers);
+//     const markdownContent = genMarkdown.generateMarkdown(answers);
+//     // console.log('markdown', markdownContent);
+
+// // Writes markdowncontent to file and logs necessary result
+//     fs.writeFile(fileName, markdownContent, (err) =>
+//     err ? console.log(err) : console.log('Successfully generated genREADME.md!')
+//     );
+// };
+
+// Function to initialize the questions for user
+function init() {
+    inquirer
+        .prompt (questions)
+        .then ((answers) => {
+            console.log('init', answers);
+            // writeToFile('genREADME.md', answers);
+        });
+};
+
+// Function call to initialize app
+init();
